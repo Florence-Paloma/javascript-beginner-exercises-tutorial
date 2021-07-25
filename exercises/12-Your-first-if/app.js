ba9a7d4936170446e62539fc49081f64725ccc5d
Florence-Paloma <f.palomagaspar@gmail.com>
2021-07-25 18:21:36 +0000

    hasta 12 hecho

--- exercises/12-Your-first-if/app.js
+++ exercises/12-Your-first-if/app.js
@@ -1,3 +1,15 @@
-var total = prompt('How many km are left to go?');
+var total = prompt("How many km are left to go?");
 
 // Your code below:
+function reply(total){
+    let answer;
+    if (total > 100) {
+    answer= "Are we there yet?";
+    } else if (total <= 100 && total > 50) {
+    answer= "We'll be there in 5 minutes";
+    } else {
+    answer= "I'm parking, I see you right now";
+    }
+    return answer;
+}
+console.log(reply(total))
